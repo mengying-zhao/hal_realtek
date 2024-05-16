6e137cdda2be15206ce101ebdb7324b2f5e496e8
Mengying Zhao <1160248418@qq.com>
2024-05-16 18:43:05 +0800

    test:test static check
    test
    
    Signed-off-by: Mengying Zhao <1160248418@qq.com>

---
+++ script/ci/ignore_check_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2018,2020 Intel Corporation
+# Copyright (c) 2022 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+ignore_file_encoding = [] #r"sdk/src/mcu/peripheral/usb",
+
+ignore_file_cpp_astyle_check = []
